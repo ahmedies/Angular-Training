5e06136bf14852b8e024222997c3d2665a33ce3f
Ahmed <ahmed.rezgui@planetgroupint.com>
2019-03-05 15:43:07 +0100

    Training Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'pgi-blog';
+  posts = [
+    {
+      title: 'Mon premier post',
+      content: 'Lorem ipsum dolor sit amet, eos accumsan tacimates ad, et unum nominavi hendrerit nam. Lorem essent volutpat has no, eius noster fuisset duo ei. Usu an cibo fabellas, ei eos falli nemore virtute. Pri cetero maluisset in.',
+      loveits: 0,
+      created_at: new Date()
+    },
+    {
+      title: 'Mon deuxième post',
+      content: 'Lorem ipsum dolor sit amet, eos accumsan tacimates ad, et unum nominavi hendrerit nam. Lorem essent volutpat has no, eius noster fuisset duo ei. Usu an cibo fabellas, ei eos falli nemore virtute. Pri cetero maluisset in.',
+      loveits: 0,
+      created_at: new Date()
+    },
+    {
+      title: 'Encore un post',
+      content: 'Lorem ipsum dolor sit amet, eos accumsan tacimates ad, et unum nominavi hendrerit nam. Lorem essent volutpat has no, eius noster fuisset duo ei. Usu an cibo fabellas, ei eos falli nemore virtute. Pri cetero maluisset in.',
+      loveits: 0,
+      created_at: new Date()
+    }
+  ];
+  constructor() {
+    
+  }
+  
 }
